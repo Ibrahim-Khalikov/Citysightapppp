a86a6be545285fd9224cc97c837fb1edc2e706ae
Ibrahim Khalikov <starlink050@gmail.com>
2022-01-08 20:53:30 +0500

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by cloud_vfx on 08/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
---
+++ CitySightsApp/Views/LaunchView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CitySightsApp
+//
+//  Created by cloud_vfx on 08/01/22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Heldfasdflo, world!")
+            .padding()
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
